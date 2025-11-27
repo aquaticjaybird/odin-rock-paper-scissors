a0961319d67599cd5c660fe40fed97267fcb38be
aquaticjaybird <aquaticjaybird@gmail.com>
2025-11-27 15:39:22 +0000

    Add functional base UI

--- script.js
+++ script.js
@@ -1,59 +1,62 @@
-function getComputerChoice() {
-    const randNumber = Math.floor((Math.random() * 3));
-    let choiceString;
-    switch(randNumber) {
-        case 0:
-            choiceString = 'rock';
-            break;
-        case 1:
-            choiceString = 'paper';
-            break;
-        case 2:
-            choiceString = 'scissors';
-            break;
-    }
-    return choiceString;
-}
-
-function getHumanChoice() {
-    const inputString = prompt('Select "rock", "paper" or "scissors": ');
-    return inputString.toLowerCase();
-}
-
 function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+    function getComputerChoice() {
+        const randNumber = Math.floor((Math.random() * 3));
+        let choiceString;
+        switch(randNumber) {
+            case 0:
+                choiceString = 'Rock';
+                break;
+            case 1:
+                choiceString = 'Paper';
+                break;
+            case 2:
+                choiceString = 'Scissors';
+                break;
+        }
+        return choiceString;
+    }
+    function appendRoundResult(resultText) {
+        const lastRoundResult = document.createElement("p");
+        lastRoundResult.textContent = resultText;
+        roundResultDisplay.appendChild(lastRoundResult);
+    }
     function playRound(humanChoice, computerChoice) {
         if (humanChoice === computerChoice) {
-            console.log(`It's a draw!(${humanChoice})`);
+            appendRoundResult(`It's a draw!(${humanChoice})`);
         } else if (
-          (humanChoice === 'rock' && computerChoice === 'scissors') ||
-          (humanChoice === 'paper' && computerChoice === 'rock') ||
-          (humanChoice === 'scissors' && computerChoice === 'paper')
+          (humanChoice === 'Rock' && computerChoice === 'Scissors') ||
+          (humanChoice === 'Paper' && computerChoice === 'Rock') ||
+          (humanChoice === 'Scissors' && computerChoice === 'Paper')
           ) {
-            console.log(`${humanChoice} beats ${computerChoice}! Human wins.`);
+            appendRoundResult(`${humanChoice} beats ${computerChoice}! Human wins.`);
             humanScore++;
+            humanScoreDisplay.textContent = humanScore;
         } else if (
-          (computerChoice === 'rock' && humanChoice === 'scissors') ||
-          (computerChoice === 'paper' && humanChoice === 'rock') ||
-          (computerChoice === 'scissors' && humanChoice === 'paper')
+          (computerChoice === 'Rock' && humanChoice === 'Scissors') ||
+          (computerChoice === 'Paper' && humanChoice === 'Rock') ||
+          (computerChoice === 'Scissors' && humanChoice === 'Paper')
           ) {
-            console.log(`${computerChoice} beats ${humanChoice}! Computer wins.`);
+            appendRoundResult(`${computerChoice} beats ${humanChoice}! Computer wins.`);
             computerScore++;
+            computerScoreDisplay.textContent = computerScore;
         } else {
             console.log(`Invalid input! Computer wins(${computerChoice}).`);
             computerScore++;
         }
     }
 
-    for (let roundNumber = 1; roundNumber <= 5; roundNumber++) {
-        console.log(`Round ${roundNumber}: `);
-        playRound(getHumanChoice(),getComputerChoice());
-    }
-    if (humanScore > computerScore) {
-        console.log(`Human wins the game!(${humanScore}>${computerScore})`)
-    } else {
-        console.log(`Computer wins the game!(${computerScore}>${humanScore})`)
-    }
+    let humanScore = 0;
+    let computerScore = 0;
+    const humanScoreDisplay = document.querySelector(".human-score");
+    const computerScoreDisplay = document.querySelector(".computer-score");
+    
+    const roundResultDisplay = document.querySelector(".round-result-container");
+
+    const choiceContainer = document.querySelector(".choice-container");
+    choiceContainer.addEventListener("click", (event) => {
+        const humanChoice = event.target.textContent;
+        const computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+        });
 }
 playGame();
